b8c32b41673da6301c51f4bc456f5f04834e7569
yathuwaran <yathuwar@stud.ntnu.no>
2020-03-19 15:08:33 +0100

    Mnist task finished

--- MNist/MNist_ttt4275/NN_classifier.m
+++ MNist/MNist_ttt4275/NN_classifier.m
@@ -5,22 +5,20 @@ chunk_size = 1000;
 chunks = num_test/chunk_size; %Number of chunks
 NN_dist = zeros(num_test,chunks);
 NN_indices = zeros(num_test,chunks);
-
-%% Go through each chunk and sample to find nearest neighbour(NN)
-
+NN_tags = zeros(num_test,1);
+%% Locate nearest neighbour(NN) in each chunk
 for i = 1:chunks
-    W = trainv((i-1)*chunk_size+1:i*chunk_size,:);
+    W = trainv((i-1)*chunk_size+1:i*chunk_size,:);      %Using training set
     EUC = dist(W,testv'); %Euclidean distance between W and testv
     for j = 1:num_test
         [NN_distance, NN_index] = min(EUC(:,j));
         NN_dist(j,i) = NN_distance;
         NN_indices(j,i) = (i-1)*chunk_size+NN_index;
     end
-    fprintf("Iterated through chunk %d\n",i)
+    fprintf("Iterated through chunk %d/10\n",i)
 end
 
-%% Go through each sample to find global nearest neighbour(GNN)
-NN_tags = zeros(num_test,1);
+%% Locate global nearest neighbour(G-NN) for each sample
 
 for i= 1:num_test
     [NN_distance, NN_index] = min(NN_dist(i,:));
@@ -29,3 +27,12 @@ for i= 1:num_test
 end
 
 toc
+
+%% Calculate Confusion Matrix and Error Rate
+confusion_matrix = zeros(classes);
+for i = 1:num_test
+    confusion_matrix(NN_tags(i)+1,testlab(i)+1) = confusion_matrix(NN_tags(i)+1,testlab(i)+1)+1;
+end
+error_rate = 1 - trace(confusion_matrix)/num_test;
+plt_confusion;
+NN_plotting;
